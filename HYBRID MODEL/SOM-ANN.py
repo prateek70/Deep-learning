3d90f9629cd998551f8b2ebb8213fa96a136c75b
prateek lohchub <plohchub@gmail.com>
2023-10-19 19:28:47 +0530

    Add files via upload

---
+++ HYBRID MODEL/SOM-ANN.py
@@ -0,0 +1,67 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset =  pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:,  :-1].values
+y = dataset.iloc[:, -1].values
+
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range=(0, 1))
+X = sc.fit_transform(X)
+
+
+from minisom import MiniSom
+som = MiniSom(x = 10, y = 10, input_len= 15, sigma= 1.0, learning_rate= 0.5, random_seed= 2)
+som.random_weights_init(X)
+som.train_random(X, num_iteration = 100)
+
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map())
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[y[i]],
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize = 10,
+         markeredgewidth = 2)
+
+mappings = som.win_map(X)
+frauds = np.concatenate((mappings[(3,8)], mappings[(8,2)]), axis = 0)
+frauds = sc.inverse_transform(frauds)
+
+customers = dataset.iloc[:, 1:].values
+
+is_fraud = np.zeros(len(dataset))
+for i in range(len(dataset)):
+    if dataset.iloc[i,0] in frauds:
+        is_fraud[i] = 1
+ 
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+customers = sc.fit_transform(customers)
+
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()
+
+classifier.add(Dense(units = 2 , kernel_initializer = 'uniform', activation = 'relu', input_dim = 15))
+classifier.add(Dense(units = 1 , kernel_initializer = 'uniform', activation = 'sigmoid'))
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.fit(customers, is_fraud, batch_size = 1, epochs = 2)
+
+y_pred = classifier.predict(customers)
+
+y_pred = np.concatenate((dataset.iloc[:, 0:1].values, y_pred),axis = 1)
+    
+y_pred = y_pred[y_pred[:, 1].argsort()]
